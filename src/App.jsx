d284999c5c026c26bdbecbae95f84b14c4071bd0
JuliaTiunova <juliatunn@gmail.com>
2021-06-30 16:47:37 +0300

    add background to title page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import styles from "./App.module.css";
 
 function App() {
-  return null;
+  function getGreeting(user) {
+    if (user) {
+      return `Hello, {user}`;
+    }
+    return `Welcome, dreamer!`;
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>{getGreeting()}</h1>
+    </div>
+  );
 }
 
 export default App;
