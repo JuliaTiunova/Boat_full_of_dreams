bcbd1f795efa5e787c4619fa3b39b2bf88ffcd51
JuliaTiunova <juliatunn@gmail.com>
2021-07-24 13:53:35 +0300

    Updates

--- src/functions/findRandomElement.js
+++
@@ -1,9 +0,0 @@
-import findRandomIndex from "./findRandomIndex";
-
-const findRandomElement = (arr) => {
-  const index = findRandomIndex(arr.length);
-  const element = arr[index];
-  return element;
-};
-
-export default findRandomElement;
